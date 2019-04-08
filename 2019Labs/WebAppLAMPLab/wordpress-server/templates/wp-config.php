db2b04d8f4cb88b78c3ca4ca01f116ae82258bd8
Kevin Holmes <kev@gokev.com>
2019-04-08 16:44:12 -0400

    aws inventory

--- 2019Labs/WebAppLAMPLab/wordpress-server/templates/wp-config.php
+++ 2019Labs/WebAppLAMPLab/wordpress-server/templates/wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', '{{ wpdbuser }}');
 define('DB_PASSWORD', '{{ wpdbpass }}');
 
 /** MySQL hostname */
-define('DB_HOST', '{{ hostvars['lampweb']['ansible_facts']['eth0']['ipv4']['address'] }}');
+define('DB_HOST', '{{ hostvars['lampdb']['ansible_facts']['eth0']['ipv4']['address'] }}');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
