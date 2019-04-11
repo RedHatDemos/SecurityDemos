c4ac504f4b044270a9a611e7590b6b05284c3b89
Kevin Holmes <kev@gokev.com>
2019-04-11 11:02:28 -0400

    catsploit!

--- 2019Labs/WebAppLAMPLab/roles/wordpress-server/templates/cat_meme_takeover.sh
+++ 2019Labs/WebAppLAMPLab/roles/wordpress-server/templates/cat_meme_takeover.sh
@@ -1,21 +1,39 @@
 #!/bin/sh
 
-mysql -h {{ dbhostname }} -u insecure {{ wpdbname }} < /tmp/cat_meme_takeover.sql
+mysql -h {{ dbhostname }} -u insecure {{ wpdbname }} < cat_meme_takeover.sql > /dev/null 2>&1
 
-echo
-echo
-echo '      ##############################'
-echo '       __  __ _____ _____        __ '
-echo '      |  \/  | ____/ _ \ \      / / '
-echo '      | |\/| |  _|| | | \ \ /\ / /  '
-echo '      | |  | | |__| |_| |\ V  V /   '
-echo '      |_|  |_|_____\___/  \_/\_/    '
-echo '   	       	       	       	  '
-echo '      ##############################'
-echo '                                    '
-echo '      DONE!  Now reload the web page'
-echo '         http://{{ webhostname }}    '
-echo ' to see what the evil cat hacker clan did!'
-echo
-echo
+if [ $? = 0 ]; then
+	echo
+	echo
+	echo '      ##############################'
+	echo '       __  __ _____ _____        __ '
+	echo '      |  \/  | ____/ _ \ \      / / '
+	echo '      | |\/| |  _|| | | \ \ /\ / /  '
+	echo '      | |  | | |__| |_| |\ V  V /   '
+	echo '      |_|  |_|_____\___/  \_/\_/    '
+	echo '   	       	       	       	  '
+	echo '      ##############################'
+	echo '                                    '
+	echo '      DONE!  Now reload the web page'
+	echo '         http://{{ webhostname }}    '
+	echo ' to see what the evil cat hacker clan did!'
+	echo
+	echo
+else
+	echo
+	echo
+	echo '       ############################## '
+        echo '       ____      ___        ______  _ '
+        echo '      |  _ \    / \ \      / /  _ \| |'
+        echo '      | |_) |  / _ \ \ /\ / /| |_) | |'
+        echo '      |  _ <  / ___ \ V  V / |  _ <|_|'
+        echo '      |_| \_\/_/   \_\_/\_/  |_| \_(_)'
+	echo '   	       	       	       	    '
+	echo '       ############################## '
+	echo '                                      '
+	echo '        FAILED!  You do not can haz   '
+	echo '        permissionz to the database   '
+	echo
+	echo
+fi
 
